e1e8ef959eca64067207346e0b8a0836aeb9ae11
AleksKAG <WyrkaloW@yandex.ru>
2024-11-30 16:03:42 +0300

    Update main.go-3

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -42,29 +43,14 @@ func (s ParcelService) Register(client int, address string) (Parcel, error) {
 	if err != nil {
 		return parcel, err
 	}
-
 	parcel.Number = id
 
-	fmt.Printf("Новая посылка № %d на адрес %s от клиента с идентификатором %d зарегистрирована %s\n",
-		parcel.Number, parcel.Address, parcel.Client, parcel.CreatedAt)
-
+	fmt.Printf("Новая посылка № %d зарегистрирована на адрес %s от клиента %d.\n", parcel.Number, address, client)
 	return parcel, nil
 }
 
-func (s ParcelService) PrintClientParcels(client int) error {
-	parcels, err := s.store.GetByClient(client)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Посылки клиента %d:\n", client)
-	for _, parcel := range parcels {
-		fmt.Printf("Посылка № %d на адрес %s от клиента с идентификатором %d зарегистрирована %s, статус %s\n",
-			parcel.Number, parcel.Address, parcel.Client, parcel.CreatedAt, parcel.Status)
-	}
-	fmt.Println()
-
-	return nil
+func (s ParcelService) ChangeAddress(number int, address string) error {
+	return s.store.SetAddress(number, address)
 }
 
 func (s ParcelService) NextStatus(number int) error {
@@ -83,90 +69,63 @@ func (s ParcelService) NextStatus(number int) error {
 		return nil
 	}
 
-	fmt.Printf("У посылки № %d новый статус: %s\n", number, nextStatus)
-
+	fmt.Printf("Статус посылки № %d обновлён на %s.\n", number, nextStatus)
 	return s.store.SetStatus(number, nextStatus)
-}
-
-func (s ParcelService) ChangeAddress(number int, address string) error {
-	return s.store.SetAddress(number, address)
 }
 
 func (s ParcelService) Delete(number int) error {
 	return s.store.Delete(number)
 }
 
-func main() {
-	// настройте подключение к БД
+func (s ParcelService) PrintClientParcels(client int) error {
+	parcels, err := s.store.GetByClient(client)
+	if err != nil {
+		return err
+	}
 
-	store := // создайте объект ParcelStore функцией NewParcelStore
+	fmt.Printf("Посылки клиента %d:\n", client)
+	for _, parcel := range parcels {
+		fmt.Printf("№ %d: %s, статус: %s\n", parcel.Number, parcel.Address, parcel.Status)
+	}
+	return nil
+}
+
+func main() {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	store := NewParcelStore(db)
 	service := NewParcelService(store)
 
-	// регистрация посылки
 	client := 1
-	address := "Псков, д. Пушкина, ул. Колотушкина, д. 5"
+	address := "Москва, ул. Ленина, 5"
+	newAddress := "Санкт-Петербург, ул. Пушкина, 10"
+
 	p, err := service.Register(client, address)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	// изменение адреса
-	newAddress := "Саратов, д. Верхние Зори, ул. Козлова, д. 25"
-	err = service.ChangeAddress(p.Number, newAddress)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := service.ChangeAddress(p.Number, newAddress); err != nil {
+		log.Println(err)
 	}
 
-	// изменение статуса
-	err = service.NextStatus(p.Number)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := service.NextStatus(p.Number); err != nil {
+		log.Println(err)
 	}
 
-	// вывод посылок клиента
-	err = service.PrintClientParcels(client)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := service.PrintClientParcels(client); err != nil {
+		log.Println(err)
 	}
 
-	// попытка удаления отправленной посылки
-	err = service.Delete(p.Number)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := service.Delete(p.Number); err != nil {
+		log.Println(err)
 	}
 
-	// вывод посылок клиента
-	// предыдущая посылка не должна удалиться, т.к. её статус НЕ «зарегистрирована»
-	err = service.PrintClientParcels(client)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// регистрация новой посылки
-	p, err = service.Register(client, address)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// удаление новой посылки
-	err = service.Delete(p.Number)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// вывод посылок клиента
-	// здесь не должно быть последней посылки, т.к. она должна была успешно удалиться
-	err = service.PrintClientParcels(client)
-	if err != nil {
-		fmt.Println(err)
-		return
+	if err := service.PrintClientParcels(client); err != nil {
+		log.Println(err)
 	}
 }
